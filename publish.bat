a75c6545ac971649d1544edfc37626852eebb4c8
RealOrko <RealOrko@nowhere.com>
2019-03-14 22:11:29 +0000

    Have a sql-d.ui packaging for win-x64

--- publish.bat
+++ publish.bat
@@ -2,8 +2,6 @@
 
 CALL kill.bat
 
-@ECHO on
-
 ECHO.
 ECHO [SQL-D]:PUBLISH/
 ECHO.
@@ -14,23 +12,23 @@ IF NOT EXIST .\build ( MKDIR .\build )
 SET Configuration=Release
 SET TargetFramework=netcoreapp2.2
 
-rem SET LibProjectPath=./src/sql-d/SqlD.csproj
-rem dotnet build %LibProjectPath% || EXIT /B 1
-rem dotnet pack %LibProjectPath% -o ../../build || EXIT /B 1
-rem 	
-rem SET StartLinuxX64ProjectPath=./src/sql-d.start.linux-x64/SqlD.Start.linux-x64.csproj
-rem dotnet publish %StartLinuxX64ProjectPath% -r linux-x64 --self-contained || EXIT /B 1
-rem dotnet pack %StartLinuxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
-rem 	
-rem SET StartOsxX64ProjectPath=./src/sql-d.start.osx-x64/SqlD.Start.osx-x64.csproj
-rem dotnet publish %StartOsxX64ProjectPath% -r osx-x64 --self-contained || EXIT /B 1
-rem dotnet pack %StartOsxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
-rem 
-rem SET StartWinX64ProjectPath=./src/sql-d.start.win-x64/SqlD.Start.win-x64.csproj
-rem dotnet publish %StartWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
-rem dotnet pack %StartWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
-rem 
+SET LibProjectPath=./src/sql-d/SqlD.csproj
+dotnet build %LibProjectPath% || EXIT /B 1
+dotnet pack %LibProjectPath% -o ../../build --include-symbols || EXIT /B 1
+	
+SET StartLinuxX64ProjectPath=./src/sql-d.start.linux-x64/SqlD.Start.linux-x64.csproj
+dotnet publish %StartLinuxX64ProjectPath% -r linux-x64 --self-contained || EXIT /B 1
+dotnet pack %StartLinuxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
+	
+SET StartOsxX64ProjectPath=./src/sql-d.start.osx-x64/SqlD.Start.osx-x64.csproj
+dotnet publish %StartOsxX64ProjectPath% -r osx-x64 --self-contained || EXIT /B 1
+dotnet pack %StartOsxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
+
+SET StartWinX64ProjectPath=./src/sql-d.start.win-x64/SqlD.Start.win-x64.csproj
+dotnet publish %StartWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
+dotnet pack %StartWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
 
 SET UIWinX64ProjectPath=./src/sql-d.ui/SqlD.UI.csproj
 dotnet publish %UIWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
-dotnet pack %UIWinX64ProjectPath% -o ../../build || EXIT /B 1
+xcopy /S /Q src\sql-d.ui\bin\Release\netcoreapp2.2\Views src\sql-d.ui\bin\Release\netcoreapp2.2\win-x64\publish\Views\
+dotnet pack %UIWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
