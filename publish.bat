21b90a766ef83f1d626be386f2b251112fc589ca
RealOrko <RealOrko@nowhere.com>
2019-03-14 22:32:46 +0000

    Split ui projects into co-respective nuget runtimes

--- publish.bat
+++ publish.bat
@@ -28,7 +28,17 @@ SET StartWinX64ProjectPath=./src/sql-d.start.win-x64/SqlD.Start.win-x64.csproj
 dotnet publish %StartWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
 dotnet pack %StartWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
 
-SET UIWinX64ProjectPath=./src/sql-d.ui/SqlD.UI.csproj
+SET UILinuxX64ProjectPath=./src/sql-d.ui.linux-x64/SqlD.UI.linux-x64.csproj
+dotnet publish %UILinuxX64ProjectPath% -r linux-x64 --self-contained || EXIT /B 1
+xcopy /S /Q src\sql-d.ui.linux-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui\bin\Release\netcoreapp2.2\linux-x64\publish\Views\
+dotnet pack %UILinuxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
+
+SET UIOsxX64ProjectPath=./src/sql-d.ui.osx-x64/SqlD.UI.osx-x64.csproj
+dotnet publish %UIOsxX64ProjectPath% -r osx-x64 --self-contained || EXIT /B 1
+xcopy /S /Q src\sql-d.ui.osx-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui\bin\Release\netcoreapp2.2\osx-x64\publish\Views\
+dotnet pack %UIOsxX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
+
+SET UIWinX64ProjectPath=./src/sql-d.ui.win-x64/SqlD.UI.win-x64.csproj
 dotnet publish %UIWinX64ProjectPath% -r win-x64 --self-contained || EXIT /B 1
-xcopy /S /Q src\sql-d.ui\bin\Release\netcoreapp2.2\Views src\sql-d.ui\bin\Release\netcoreapp2.2\win-x64\publish\Views\
+xcopy /S /Q src\sql-d.ui.win-x64\bin\Release\netcoreapp2.2\Views src\sql-d.ui\bin\Release\netcoreapp2.2\win-x64\publish\Views\
 dotnet pack %UIWinX64ProjectPath% -o ../../build --include-symbols || EXIT /B 1
