3b5ec1433c89e62006b4ded4c82c98c1b503b923
Gavin van der Merwe <gavin@lemonedge.com>
2024-10-01 18:56:29 +0100

    Adding font-awesome

---
+++ src/sql-d.ui/Controllers/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SqlD.Network;
+using SqlD.UI.Models.Services;
+using SqlD.UI.Services;
+
+namespace SqlD.UI.Controllers;
+
+public class SettingsController : Controller
+{
+    private readonly SettingsService _settings;
+
+    public SettingsController(SettingsService services)
+    {
+        _settings = services;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
