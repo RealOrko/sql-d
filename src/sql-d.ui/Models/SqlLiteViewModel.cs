bdebe2b4836e00c07011fb58bc717494e62e529d
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:45 +0100

    Have different lifecycle strategies running

--- src/sql-d.ui/Models/SqlLiteViewModel.cs
+++ src/sql-d.ui/Models/SqlLiteViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using SqlD.UI.Models.Query;
 
 namespace SqlD.UI.Models;
@@ -6,6 +7,7 @@ public class SqlLiteViewModel
 {
     public string Server { get; set; }
     public string Query { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Now;
     public QueryResultViewModel QueryResult { get; set; }
     public CommandResultViewModel CommandResult { get; set; }
     public DescribeResultViewModel DescribeResult { get; set; }
--- src/sql-d/Configs/Model/SqlDSettings.cs
+++ src/sql-d/Configs/Model/SqlDSettings.cs
@@ -2,6 +2,7 @@ namespace SqlD.Configs.Model;
 
 public class SqlDSettings
 {
+    public SqlDSettingsConnections Connections { get; set; } = new();
     public SqlDSettingsReplication Replication { get; set; } = new();
     public SqlDSettingsForwarding Forwarding { get; set; } = new();
 }
