26c971056e2f279962bbd5c10a911b588c149e4c
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:47 +0100

    Adding dotnet tools for creating packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,6 +12,11 @@ echo ''
 echo '[SQL-D]:BUILD/'
 echo ''
 
+dotnet tool install --global dotnet-zip
+dotnet tool install --global dotnet-tarball
+dotnet tool install --global dotnet-rpm
+dotnet tool install --global dotnet-deb
+
 dotnet restore $PWD/tests/sql-d/SqlD.Tests.csproj
 dotnet build $PWD/tests/sql-d/SqlD.Tests.csproj
 dotnet test $PWD/tests/sql-d/SqlD.Tests.csproj --logger "trx;LogFileName=test-results.trx"
