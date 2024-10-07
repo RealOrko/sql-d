198c423be6c1cc3c2e75c3f52576dd4446779647
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:45 +0100

    Fixing configs for test project

--- tests/sql-d/Framework/BaseTestCase.cs
+++ tests/sql-d/Framework/BaseTestCase.cs
@@ -2,10 +2,8 @@ namespace SqlD.Tests.Framework;
 
 public class BaseTestCase
 {
-    protected const string ConfigurationFileName = "appsettings.json";
-
     static BaseTestCase()
     {
-        Interface.Setup(typeof(NetworkTestCase).Assembly, ConfigurationFileName);
+        Interface.Setup(typeof(BaseTestCase).Assembly, "appsettings.json");
     }
 }
