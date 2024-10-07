12d48bcaa8157019aa8610ff2419370b6665385a
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:48 +0100

    Making packages more relevant

--- scripts/package.sh
+++ scripts/package.sh
@@ -26,8 +26,6 @@ dotnet restore $PWD/src/sql-d/SqlD.csproj
 dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION 
 dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 
 dotnet restore $PWD/src/sql-d.ui/SqlD.UI.csproj
 dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION 
