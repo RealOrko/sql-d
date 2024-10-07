96d1f2225129e8a7b5d52b5d75763dd4ccf48403
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:47 +0100

    Got versioning sorted

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,22 +12,27 @@ echo ''
 echo '[SQL-D]:PUBLISH/'
 echo ''
 
+# For testing locally, please commit GITHUB_RUN_NUMBER commented 
 export GITHUB_RUN_NUMBER='12'
-export GITHUB_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+export GITHUB_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | sed 's/master//g' | sed 's/main//g')
+export SQLD_VERSION=$(echo "$GITHUB_RUN_NUMBER-$GITHUB_BRANCH_NAME" | sed 's/\-$//g')
 
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER 
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
+git clean -x -f -d
 
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER 
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$GITHUB_RUN_NUMBER
+dotnet restore $PWD/src/sql-d/SqlD.csproj
+dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION 
+dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d/SqlD.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 
-rm -rf $PWD/published/
+dotnet restore $PWD/src/sql-d.ui/SqlD.UI.csproj
+dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION 
+dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+
 mkdir -p $PWD/published/
 
 find $PWD/src -name '*.nupkg' -exec cp "{}" $PWD/published/  \;
