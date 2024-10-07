a7fe8005337e23c7b61237ea686641273b487289
Gavin van der Merwe <gavin@lemonedge.com>
2024-10-07 16:14:39 +0100

    Adding install script for packages

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DOTNETPATH=$(which dotnet)
+if [ ! -f "$DOTNETPATH" ]; then
+	echo "Please install Microsoft/dotnetcore from: https://www.microsoft.com/net/core"
+	exit 1
+fi
+
+echo ''
+echo '[SQL-D]:INSTALL/'
+echo ''
+
+sudo dpkg --purge sqld.ui || true
+sudo dpkg -i $PWD/SqlD.UI.1.0.0-refactor.deb
+sudo systemctl enabled sqld.ui
+sudo systemctl start sqld.ui
