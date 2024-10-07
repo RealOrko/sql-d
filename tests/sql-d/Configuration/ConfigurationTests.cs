17bd75b644f6430a63e5bd4f7b393d8f9757ad6a
RealOrko <realorko@nowhere.com>
2024-10-07 18:14:42 +0100

    Tests passing

--- tests/sql-d/Configuration/ConfigurationTests.cs
+++ tests/sql-d/Configuration/ConfigurationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using SqlD.Network.Diagnostics;
 using SqlD.Tests.Framework.Network;
 
@@ -12,15 +13,15 @@ namespace SqlD.Tests.Configuration
 		public void ShouldBeAbleToStartASqlDConfiguration()
 		{
 			var sqlD = GetType().Assembly.SqlDGo(@"./Configuration/appsettings.tests.json");
-			Assert.NotNull(sqlD);
+			ClassicAssert.NotNull(sqlD);
 
 			var registry = sqlD.Services.First(x => x.Name.Equals("sql-d-registry-1"));
-			Assert.Contains("registry", registry.Tags);
+			ClassicAssert.Contains("registry", registry.Tags);
 
             EndPointMonitor.WaitUntil(registry.ToEndPoint(), EndPointIs.Up);
 
 			var master = sqlD.Services.First(x => x.Name.Equals("sql-d-master-1"));
-			Assert.Contains("master", master.Tags);
+			ClassicAssert.Contains("master", master.Tags);
 
             EndPointMonitor.WaitUntil(master.ToEndPoint(), EndPointIs.Up);
         }
