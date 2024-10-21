53de40369073369f40c2b49ba759c67ae3aebc50
RealOrko <realorko@nowhere.com>
2024-10-21 18:49:07 +0100

    Moving ports around so things work with docker containers

--- scripts/build.sh
+++ scripts/build.sh
@@ -30,4 +30,3 @@ dotnet tool install --global dotnet-deb
 dotnet restore $PWD/tests/sql-d/SqlD.Tests.csproj
 dotnet build $PWD/tests/sql-d/SqlD.Tests.csproj
 dotnet test $PWD/tests/sql-d/SqlD.Tests.csproj --logger "trx;LogFileName=test-results.trx"
-
