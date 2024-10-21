5b9cb06fc6a03b9bbc0851a10ae5a9a8dc63a0b9
RealOrko <realorko@nowhere.com>
2024-10-21 18:36:00 +0100

    Have basic image build scripts working, can connect from systemd installed UI

---
+++ scripts/docker-build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+DOTNETPATH=$(which dotnet)
+if [ ! -f "$DOTNETPATH" ]; then
+	echo "Please install Microsoft/dotnetcore from: https://www.microsoft.com/net/core"
+	exit 1
+fi
+
+set +u
+
+if [ -z "$GITHUB_RUN_NUMBER" ]; then
+	export GITHUB_RUN_NUMBER=0
+fi 
+
+set -u
+
+echo ''
+echo '[SQL-D]:BUILD/'
+echo ''
+echo "Current Run Number: $GITHUB_RUN_NUMBER"
+echo ''
+
+docker build -f $PWD/src/sql-d.cli/Dockerfile -t sql-d:latest . 
