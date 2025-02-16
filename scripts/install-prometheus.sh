528da68bea8841c54a720c0d719569af94396bff
RealOrko <realorko@nowhere.com>
2025-02-16 17:01:28 +0000

    Got aspnet dashboards working

--- scripts/install-prometheus.sh
+++ scripts/install-prometheus.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
+kubectl -n default apply -f $PWD/scripts/dashboards/aspnetcore.yaml
+kubectl -n default apply -f $PWD/scripts/dashboards/aspnetcore-endpoints.yaml
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# helm install prometheus prometheus-community/kube-prometheus-stack --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false 
 helm install prometheus prometheus-community/kube-prometheus-stack
