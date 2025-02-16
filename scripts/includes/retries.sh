0d750c966c1ee879edcab8d41b2be43b58c23add
RealOrko <realorko@nowhere.com>
2025-02-16 17:42:56 +0000

    Adding retries for rpm package because it keeps failing for some reason

---
+++ scripts/includes/retries.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function retry {
+  local n=1
+  local max=5
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "Command failed. Attempt $n/$max:"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
--- scripts/package.sh
+++ scripts/package.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+. $PWD/scripts/includes/retries.sh
+
 DOTNETPATH=$(which dotnet)
 if [ ! -f "$DOTNETPATH" ]; then
 	echo "Please install Microsoft/dotnetcore from: https://www.microsoft.com/net/core"
@@ -39,7 +41,7 @@ dotnet restore $PWD/src/sql-d.ui/SqlD.UI.csproj
 dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:Build /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION 
 dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateZip /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateTarball /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
-dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
+retry dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateRpm /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 dotnet msbuild $PWD/src/sql-d.ui/SqlD.UI.csproj /t:CreateDeb /p:Configuration=Release /p:RevisionVersion=$SQLD_VERSION
 
 mkdir -p $PWD/packages/
