7a00a2823f7cc1f5638408b6f5180f9ca4d0af06
RealOrko <realorko@nowhere.com>
2025-02-16 11:58:11 +0000

    Added open telemetry/prometheus

--- src/sql-d/Network/Server/ConnectionListenerStartup.cs
+++ src/sql-d/Network/Server/ConnectionListenerStartup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.Features;
+using OpenTelemetry.Metrics;
 using SqlD.Network.Server.Middleware;
 using SqlD.Network.Server.Workers;
 
@@ -10,6 +11,19 @@ internal class ConnectionListenerStartup
 
     public void ConfigureServices(IServiceCollection services)
     {
+        services.AddOpenTelemetry()
+            .WithMetrics(builder =>
+            {
+                builder.AddPrometheusExporter();
+
+                builder.AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel");
+                builder.AddView("http.server.request.duration",
+                    new ExplicitBucketHistogramConfiguration
+                    {
+                        Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10]
+                    });
+            });
+        
         services.Configure<FormOptions>(options =>
         {
             options.BufferBody = false;
@@ -55,5 +69,6 @@ internal class ConnectionListenerStartup
 
         app.UseOpenApi();
         app.UseSwaggerUi();
+        app.UseOpenTelemetryPrometheusScrapingEndpoint();
     }
 }
