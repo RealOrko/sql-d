904b49351faf65ba22403edb130203af89b9276f
RealOrko <realorko@nowhere.com>
2025-02-17 04:37:53 +0000

    Refactoring scripts to be more useful

--- scripts/sysd-install-debian.sh
+++ scripts/sysd-install-debian.sh
@@ -7,7 +7,7 @@ echo '[SQL-D]:INSTALL/SQLD/DEBIAN/SYSTEMD'
 echo ''
 
 sudo dpkg --purge sqld.ui || true
-sudo dpkg -i $PWD/packages/SqlD.UI.1.1.0-helm.deb
+sudo dpkg -i $PWD/packages/SqlD.UI.1.1.0.deb
 sudo systemctl enable sqld.ui
 sudo systemctl start sqld.ui
 sudo systemctl status sqld.ui
